25cfb98a57ffb436cf383f73548ca26cbfaa9f29
Lowz13 <humbertomedina54@aragon.unam.mx>
2025-10-05 15:30:08 -0600

    Descirfrado casi completo

--- Clases/descifra.py
+++ Clases/descifra.py
@@ -13,6 +13,7 @@ def descifra_sustitucion(cadena, llave):
             cifrado += letra
     return cifrado
 
+
 # ============================
 # DICIONARIO
 # ============================
@@ -21,6 +22,7 @@ def carga_diccionario(ruta):
     texto = arch.read()
     arch.close()
     return texto.split()
+
 
 # ============================
 # PRIMERAS PALABRAS
@@ -39,7 +41,7 @@ def coleccion_de_letras(cadena, adicionales):
                 mejoresLetras.append(i)
         contador += 1
     mejoresLetras = sorted(mejoresLetras, key=len, reverse=True)[:]
-    
+
     
     while adicionales:
         extra = cadena.split()[rd.randint(0, contador-1)]
@@ -51,6 +53,19 @@ def coleccion_de_letras(cadena, adicionales):
             mejoresLetras.append(extra)
             adicionales -= 1  
               
+    return mejoresLetras
+
+# ============================
+# PALABRAS COMPLETAS
+# ============================
+
+
+
+
+
+
+def palabras_adicionales(cadena, adicionales):
+    mejoresLetras = coleccion_de_letras(cadena, adicionales)
     return mejoresLetras
 
 # ============================
@@ -73,21 +88,26 @@ def buscar_letras():
     posiblesLetras = ['' for i in range(26)]
     return posiblesLetras
 
+
 # ============================
 # FUNCION PRINCIPAL
 # ============================
-
 def inicio_descifrado (diccionario, palabra, alfabetoCifrado):
     #acumuladores
     coincidencias = 0
     pruebaCoincidencias = ''
+    externo = {}
+    
+    
     
     #conseguir las letras repetidas
     lector = leer_palabra_repetida(palabra)
     indexLetrasRepetidas = lector[0]
     
+    
     # Convertir patrones a pares de posiciones que deben ser iguales
     posiciones_iguales = []
+    
     
     for patron_letras in indexLetrasRepetidas:
         # Para cada grupo de letras repetidas (ej: [0,2,4] para 'a')
@@ -96,6 +116,7 @@ def inicio_descifrado (diccionario, palabra, alfabetoCifrado):
             for j in range(i + 1, len(patron_letras)):
                 posiciones_iguales.append((patron_letras[i], patron_letras[j]))
     
+    
     print("="*20)
     print(f"Palabra: {palabra}")
     print(f"Patrones encontrados: {indexLetrasRepetidas}")
@@ -103,26 +124,70 @@ def inicio_descifrado (diccionario, palabra, alfabetoCifrado):
     print("="*20)
     
     
-    for limpieza in palabra:
-        alfabetoCifrado[limpieza] = ''
+    for limpieza in set(palabra):
+        externo[limpieza] = ''
+    
         
     for p in diccionario:
         #condicion de longitud y caracteres unicos
+        
         if len(p) == len(palabra) and len(set(p)) == len(set(palabra)):
             # Verificar si las letras en las posiciones especificadas son iguales
+            # primer filtrado
             if all(p[pos1] == p[pos2] for pos1, pos2 in posiciones_iguales 
                    if pos1 < len(p) and pos2 < len(p)):
+                #Segundo filtrado
+                for patron in range(len(palabra)):
+                    bandera = False
+                    #print('revisando:', p[patron],'en',palabra[patron], alfabetoCifrado[palabra[patron]])
+                    if p[patron] in alfabetoCifrado[palabra[patron]]:
+                        bandera = True
+                        
+                    if bandera == False:
+                        #print(p,'no cumple con\n')
+                        break
                 
-                coincidencias += 1
-                print('posible:', p)
+                if bandera == True:
+                    #print(p,'\n')
+                    coincidencias += 1
+               
+
+                #print('posible:', p)
+
+                #print('posible:', p)
                 
                 for c in range(len(palabra)):
-                    if not (p[c] in alfabetoCifrado[palabra[c]]):
-                        
-                        alfabetoCifrado[palabra[c]] += p[c]
-                
+                    if not (p[c] in externo[palabra[c]]) and bandera == True:
+                        externo[palabra[c]] += p[c]
+
+
+    for c in set(palabra):
+        #print (c)
+        if len(externo[c]) == len(alfabetoCifrado[c]):
+            print("iguales",c, externo[c], alfabetoCifrado[c])
+        if len(externo[c]) > len(alfabetoCifrado[c]) and coincidencias > 0:
+            print("error",c, externo[c], alfabetoCifrado[c])
+        if len(externo[c]) < len(alfabetoCifrado[c]) and coincidencias > 0:
+            print("cambios",c, externo[c], alfabetoCifrado[c])
+            alfabetoCifrado[c] = externo[c]
+        
+
+
+    '''
+    c = 0
+    prueba = ''
+    for p in dic1:
+        if len(p) == 4 and len(set(p))==4:
+            if p[1] in posD and p[2] in posI and p[3] in posQ:
+                print(p)
+                if not(p[-1] in prueba):
+                    prueba += p[-1]
+                c = c + 1
+    '''           
     print('cumple:', coincidencias)
     print(pruebaCoincidencias)
+    
+    
     for i in alfabetoCifrado:
         print(i, alfabetoCifrado[i])
 
@@ -130,6 +195,8 @@ def inicio_descifrado (diccionario, palabra, alfabetoCifrado):
     
     return alfabetoCifrado
 
+
+# ============================
 numeros = '0123456789'
 
 alfabeto = 'abcdefghijklmnopqrstuvwxyz'
@@ -161,11 +228,10 @@ for i in range(len(coleccion[0])):
     contador += 1
 alfabetoCifrado = buscar_letras()
 '''
-  
 
+for repeticion in range(5):
+    for i in range(len(coleccion)):
+    #for i in range(3):
+        descifraA = inicio_descifrado(diccionario, coleccion[i], alfabetoCifrado)
+        alfabetoCifrado = descifraA
 
-
-    #for i in range(len(coleccion)):
-i=0
-descifraA = inicio_descifrado(diccionario, coleccion[i], alfabetoCifrado)
-    
